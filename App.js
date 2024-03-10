a337ff305723ae18a8b1b16396bb6ab6d53923e1
Kondaveeti Beulah <138322751+kondaveetiBeulah@users.noreply.github.com>
2024-03-11 01:41:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import './App.css'
-import ProductsDetails from './Components/ProductsDetails'
+
 import {useState, useEffect} from 'react'
 
-
-const App = () => {
-    const [fake,setFake] = useState([]);
-    console.log(fake);
-
-    useEffect(() =>{
-      fakeStore();
-    },[])
-  
+const App = () =>{
+  //https://fakestoreapi.com/products
+  const [fake,setFake] = useState([]);
+  console.log(fake)
+  useEffect(() =>{
+    fakeStore();
+  },[])
   const fakeStore = async() =>{
     const response = await fetch("https://fakestoreapi.com/products");
     const jsonData = await response.json()
-    const updatedData = fetchedData.products.map(product => ({
-      title: product.title,
-      brand: product.brand,
-      price: product.price,
-      id: product.id,
-      imageUrl: product.image_url,
-      rating: product.rating,
-    }))
-    setFake(updatedData)
+    setFake(jsonData)
+  }
+  
   return(
+
     <>
       <h2>Fake API Store</h2>
-      <ul className="products-list">
-          {productsList.map(product => (
-            <ProductsDetails productData={product} key={product.id} />
-          ))}
-        </ul>
-      </>
+      <div className="container">
+        {fake.map((values)=>{
+          return(
+            <>
+            <div className="box">
+          <div className="content">
+            <h5>{values.title}</h5>
+            <img src={values.image} alt=""/>
+            <p>{values.description}</p>
+          </div>
+        </div>
+            </>
+          )
+        })}
+  
+      </div>
+    </>
+
   )
 }
-
 export default App
